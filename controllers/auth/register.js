9e3ca814fe1b11c9d6ed11a46c217bdb00db16bf
Anastasiia <schili@outlook.com>
2023-05-21 13:37:21 +0300

    registers with bcrypt

--- controllers/auth/register.js
+++ controllers/auth/register.js
@@ -1,3 +1,5 @@
+const bcrypt = require("bcryptjs");
+
 const {User} = require('../../models/user')
 
 const {RequestError} = require('../../helpers');
@@ -11,7 +13,10 @@ const register = async(req, res) => {
         throw RequestError(409, 'Email in use')
     };
 
-    const result = await User.create({email, password});
+    const hashPassword = await bcrypt.hash(password, 10);
+        //const passwordCompare = await bcrypt.compare(password, result)
+
+    const result = await User.create({email, password: hashPassword});
     res.status(201).json({
         email: result.email,
         subscription: result.subscription
